dcf6632b3fd7980e021ac056f30d2663211aca17
ranjith-1211 <86145187+ranjith-1211@users.noreply.github.com>
2022-09-26 16:23:35 +0530

    Add files via upload

---
+++ MarksException.java
@@ -0,0 +1,11 @@
+package com.gl.caseStudy1;
+
+public class MarksException extends RuntimeException {
+	static final long serialVersionUID=1L;
+
+	public MarksException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
